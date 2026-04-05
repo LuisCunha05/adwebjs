14257b2e10933941e11ef0b48889db8c081ebd89
LuisCunha05 <luisf.cunha05@gmail.com>
2026-04-04 22:01:03 -0400

    :bug: fix: correção cron para rodar cada hora

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -4,7 +4,7 @@ export async function register() {
   if (process.env.NEXT_RUNTIME !== 'nodejs') return
   const { runAllTasks } = await import('./tasks/cron')
   // Run a job every hour
-  new Cron('0 */1 * * * *', () => {
+  new Cron('0 */1 * * *', () => {
     runAllTasks()
   })
 }
