a0b45d1a47e324ee392de7c9d7bc53875679f54b
LuisCunha05 <luisf.cunha05@gmail.com>
2026-07-11 19:53:52 -0400

    :wrench: chore(deps): atualiza libs

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  cacheComponents: true,
 } satisfies NextConfig
 
 export default nextConfig
